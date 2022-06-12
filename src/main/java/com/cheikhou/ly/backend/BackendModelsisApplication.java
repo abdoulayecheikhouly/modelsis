ba522fe814ceb46b52a3d0ee2ad925b8665f838a
SelbyThoma <cheikhouly92@gmail.com>
2022-06-12 21:21:24 +0000

    cheikhou

--- src/main/java/com/cheikhou/ly/backend/BackendModelsisApplication.java
+++ src/main/java/com/cheikhou/ly/backend/BackendModelsisApplication.java
@@ -1,4 +1,8 @@
 package com.cheikhou.ly.backend;
+import com.cheikhou.ly.backend.entities.Product;
+import com.cheikhou.ly.backend.entities.ProductType;
+import com.cheikhou.ly.backend.entities.Role;
+import com.cheikhou.ly.backend.entities.User;
 import com.cheikhou.ly.backend.services.interfaces.ProductService;
 import com.cheikhou.ly.backend.services.interfaces.UserService;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +26,7 @@ public class BackendModelsisApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        /*
+
         ProductType t1 = productService.addTypeProduct(new ProductType(null,"Smartphone", new ArrayList<>()));
         ProductType t2 = productService.addTypeProduct(new ProductType(null,"Laptop", new ArrayList<>()));
 
@@ -33,11 +37,11 @@ public class BackendModelsisApplication implements CommandLineRunner {
         userService.addNewRole(new Role(null,"ADMIN"));
         userService.addNewRole(new Role(null,"USER"));
 
-        userService.addNewUser(new User(null,"mbodji","oussey","oussey94","pwd", new ArrayList<>()));
-        userService.addNewUser(new User(null,"mbodji","mame","safietou01","pwd", new ArrayList<>()));
-        userService.addNewUser(new User(null,"mbodji","fatou","ouss946","pwd", new ArrayList<>()));
+        userService.addNewUser(new User(null,"diop","tafa","taf","root", new ArrayList<>()));
+        userService.addNewUser(new User(null,"tall","penda","dapen","root", new ArrayList<>()));
+        userService.addNewUser(new User(null,"ba","iba","ibs","root", new ArrayList<>()));
 
-       */
+
 
         //userService.addRoleToUser("oussey94","ADMIN");
         //userService.addRoleToUser("oussey94","USER");
